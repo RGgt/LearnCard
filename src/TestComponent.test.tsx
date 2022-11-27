4519efaa03dcf22e46171906d34bde24c12c804c
RGgt <rggt.inbox@gmail.com>
2022-11-27 15:53:26 +0200

    vitest configured

---
+++ src/TestComponent.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+
+import { TestComponent } from './TestComponent';
+
+describe('Testing the TestComponent', () => {
+  it('Renders hello world', () => {
+    // Arrange
+    // Act
+    render(<TestComponent />);
+    // Assess
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Hello world!',
+    );
+  });
+});
+
+describe('Testing something else', () => {
+  it('3+5=8', () => {
+    // Arrange
+    // Act
+    render(<TestComponent />);
+    // Assess
+    expect(3 + 5).toBe(8);
+  });
+  it('5+5=11', () => {
+    // Arrange
+    // Act
+    render(<TestComponent />);
+    // Assess
+    expect(5 + 5).toBe(11);
+  });
+});
