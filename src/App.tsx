beffe46d6a0174d596658e1e336bb994862727a7
RGgt <rggt.inbox@gmail.com>
2022-11-27 13:05:02 +0200

    installed and configured linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */ // <== as i am having some test code here.
+/* eslint-disable @typescript-eslint/naming-convention */ // <== as i am having some test code here.
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
@@ -6,9 +8,9 @@ import Card from './components/card';
 import myData from '../public/decks/experiments/test-001.json';
 
 function App() {
-	const [count, setCount] = useState(0);
-	// const cardAversMd =		' # Title\nlorem **ipsum** *React-Markdown* is **Awesome**';
-	const cardAversMd_old = `
+  const [count, setCount] = useState(0);
+  // const cardAversMd = ' # Title\nlorem **ipsum** *React-Markdown* is **Awesome**';
+  const cardAversMd_old = `
 
 
 The lift coefficient ($C_L$) is a dimensionless coefficient.
@@ -22,7 +24,7 @@ The lift coefficient ($C_L$) is a dimensionless coefficient.
 [1]: http://b.org
 
  `;
-	const cardReversMd_old = ` Präsens
+  const cardReversMd_old = ` Präsens
 | Syntax        | Description |
 | :---          |       ----: |
 | Ich bin       | Wir sind    |
@@ -36,39 +38,45 @@ The lift coefficient ($C_L$) is a dimensionless coefficient.
 (^link)
 
 [^1]: Big note.
-	`;
+`;
 
-	const cardAversMd = myData.cards[0].value.avers;
-	let cardReversMd = myData.cards[0].value.reverse;
-	// cardReversMd = cardAversMd_old;
+  const cardAversMd = myData.cards[0].value.avers;
+  const cardReversMd = myData.cards[0].value.reverse;
+  // cardReversMd = cardAversMd_old;
 
-	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<Card AversMarkdown={cardAversMd} ReverseMarkdown={cardReversMd} />
-			{/* <img src="https://www.fillmurray.com/150/150" /> */}
-			{/* <strong>BILL MURRAY</strong> */}
-		</div>
-	);
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button type="button" onClick={() => setCount((cnt) => cnt + 1)}>
+          count is
+          {' '}
+          {count}
+        </button>
+        <p>
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Card AversMarkdown={cardAversMd} ReverseMarkdown={cardReversMd} />
+      {/* <img src="https://www.fillmurray.com/150/150" /> */}
+      {/* <strong>BILL MURRAY</strong> */}
+    </div>
+  );
 }
 
 export default App;
