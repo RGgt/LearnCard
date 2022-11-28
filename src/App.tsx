4466403d42b8b7c9b44730b6785c3fe48e67ab01
RGgt <rggt.inbox@gmail.com>
2022-11-28 14:15:53 +0200

    made ThemeChanger as distinct component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,10 @@
-import { useEffect } from 'react';
-import { themeChange } from 'theme-change';
-
 import './App.css';
+import { ThemeChanger } from './components/ThemeChanger';
 
 export default function App() {
-  useEffect(() => {
-    themeChange(false);
-  }, []);
   return (
     <div className="App ">
-      <select className="gradientselect" data-choose-theme>
-        <option disabled value="">
-          Pick a theme
-        </option>
-        <option value="">Default</option>
-        <option value="light">Light</option>
-        <option value="dark">dark</option>
-        <option value="cupcake">cupcake</option>
-        <option value="bumblebee">bumblebee</option>
-        <option value="emerald">emerald</option>
-        <option value="corporate">corporate</option>
-        <option value="synthwave">synthwave</option>
-        <option value="cyberpunk">cyberpunk</option>
-        <option value="valentine">valentine</option>
-        <option value="halloween">halloween</option>
-        <option value="garden">garden</option>
-        <option value="forest">forest</option>
-        <option value="aqua">aqua</option>
-        <option value="lofi">lofi</option>
-        <option value="pastel">pastel</option>
-        <option value="fantasy">fantasy</option>
-        <option value="wireframe">wireframe</option>
-        <option value="luxury">luxury</option>
-        <option value="dracula">dracula</option>
-        <option value="cmyk">cmyk</option>
-        <option value="autumn">autumn</option>
-        <option value="business">business</option>
-        <option value="acid">acid</option>
-        <option value="lemonade">lemonade</option>
-        <option value="night">night</option>
-        <option value="coffee">coffee</option>
-        <option value="winter">winter</option>
-      </select>
+      <ThemeChanger />
       <div className="dropdown">
         <label tabIndex={0} className="btn m-1">
           Click
