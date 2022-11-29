a850d53bc5226f39149650ee1e9b88f6d3835896
RGgt <rggt.inbox@gmail.com>
2022-11-29 13:26:49 +0200

    restored daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,17 +16,6 @@ module.exports = {
     require('@tailwindcss/container-queries'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
-    // require('daisyui'),
+    require('daisyui'),
   ],
-  // daisyUI config (optional)
-  // daisyui: {
-  //   styled: true,
-  //   themes: true,
-  //   base: true,
-  //   utils: true,
-  //   logs: true,
-  //   rtl: false,
-  //   prefix: '',
-  //   // themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
-  // },
 };
