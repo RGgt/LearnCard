e920a5af54d1b0636ea174bf2b429606a9095d76
RGgt <rggt.inbox@gmail.com>
2022-12-19 16:06:27 +0200

    smaller card option buttons

--- src/components/card-view/card-options-area/ButtonVoteRed.tsx
+++ src/components/card-view/card-options-area/ButtonVoteRed.tsx
@@ -5,7 +5,7 @@ export function ButtonVoteRed(props: CardOptionAreaButtonProps) {
   if (!visible) return null;
   return (
     <div
-      className="btn-outline btn-error glass btn-circle btn absolute left-1/4 h-16 w-16 shadow-lg shadow-slate-800"
+      className="btn-outline btn-error glass btn-circle btn absolute left-1/4 h-12 w-12 shadow-lg shadow-slate-800"
       role="button"
       onClick={onClick}
       onKeyPress={handleKeyPress}
